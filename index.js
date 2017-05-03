ecb378f8b8055f359cf8702dd955009f4bb426dd
jloveric <john.loverich@gmail.com>
2017-05-03 17:19:22 -0600

    Now you can access all files from index.js.

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 "use strict"
 
-module.exports = require('./Helper.js')
+module.exports = {
+	Helper : require('./Helper.js'),
+	SbEvents : require('./SbEvents.js'),
+	Logger : require('./Logger.js'),
+	Base : require('./Base.js'),
+	SingleLetterAbbreviations : require('./SingleLetterAbbreviations.js'),
+	Stopwords : require('./Stopwords.js')
+}
